b0c1ddbc48fe0b4669bfff4e5dbae87b95c13bfa
Ruben <ruben.ardao@gmail.com>
2022-11-28 22:16:36 +0000

    Half way AWS EB

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['greatkart-course-env.eba-zybcpzwm.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-course-env.eba-bjybbzb2.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
